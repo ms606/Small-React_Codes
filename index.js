36032d0632173095c341303922384088f0db4744
Mufaddal Shakir <muffi606@gmail.com>
2020-11-17 06:36:12 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom';
 class NameForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {value: 'Muffin',
+                    value2: 'Please write an essay about your favorite DOM element.'};
 
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,13 +16,15 @@ class NameForm extends React.Component {
     //First create the functions then pass the values in the state function 
     //of constructor.
     handleChange(event){
-        this.setState({value: event.target.value});
+        this.setState({value: event.target.value,
+                       value2: event.target.value2
+                       });
+        console.log(this.setState);
         // here when set state is added, it will update the constructor state value.
-        console.log(this.state);
     }
 
     handleSubmit(event){
-        alert('A name was submitted: ' + this.state.value);
+        console.log('A name was submitted: ' + this.state.value);
         event.preventDefault();
     }
 
@@ -32,6 +35,13 @@ class NameForm extends React.Component {
                     Name:
                     <input type="text" value={this.state.value} 
                                        onChange={this.handleChange}/>
+                    Further Detail:
+                    <textarea value={this.state.value2}
+                                       onChange={this.handleChange} />
+                    Pick your favourite flavor:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                                            
+                    </select>                    
                 </label>
                 <input type="submit" value="Submit" />
             </form>
