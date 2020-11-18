e3ca976a5a42ce9bc5f3960962c043c68f979fee
Mufaddal Shakir <muffi606@gmail.com>
2020-11-18 10:16:57 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ class NameForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = {value: 'Muffin',
-                    value2: 'Please write an essay about your favorite DOM element.'};
+                    value2: 'Please write an essay about your favorite DOM element.',
+                    listFruit: ''};
 
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.listHandleChange = this.listHandleChange.bind(this);
 
     };
     
@@ -19,13 +21,27 @@ class NameForm extends React.Component {
         this.setState({value: event.target.value,
                        value2: event.target.value2
                        });
-        console.log(this.setState);
+        //console.log(this.setState);
         // here when set state is added, it will update the constructor state value.
     }
 
+
+    listHandleChange(event){
+        this.setState({listFruit: event.target.value})  
+        //console.log(event.target.value)
+        ReactDOM.render(event.target.value, document.getElementById('FruitList')) ;
+
+    }
+
     handleSubmit(event){
-        console.log('A name was submitted: ' + this.state.value);
+        
+        const element = (
+        <div>
+         <h1> A name was submitted:  + {this.state.value}</h1>
+        </div>
+        );
         event.preventDefault();
+        ReactDOM.render(element, document.getElementById('message'));
     }
 
     render(){
@@ -35,14 +51,20 @@ class NameForm extends React.Component {
                     Name:
                     <input type="text" value={this.state.value} 
                                        onChange={this.handleChange}/>
+                    <br></br>
                     Further Detail:
                     <textarea value={this.state.value2}
                                        onChange={this.handleChange} />
+                    <br></br>
                     Pick your favourite flavor:
-                    <select value={this.state.value} onChange={this.handleChange}>
-                                            
+                    <select value={this.state.listFruit} onChange={this.listHandleChange}>
+                        <option value="grapefruit">Grape Fruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="Mango">Mango</option>
                     </select>                    
                 </label>
+
                 <input type="submit" value="Submit" />
             </form>
         )
@@ -50,6 +72,6 @@ class NameForm extends React.Component {
 }
 
 ReactDOM.render(
-    <NameForm />,
+    <NameForm  />,
     document.getElementById('root')
 );
