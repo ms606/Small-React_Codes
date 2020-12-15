c27bcbd14d0dc3a8d660825d9a08fe992a441356
Mufaddal Shakir <muffi606@gmail.com>
2020-12-15 17:39:17 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = 'mongodb+srv://todo:todo123@cluster-1.00leg.mongodb.net/todo?retryWrites=true&w=majority';
 
+	
+console.log('connecting to Database..');
+
+MongoClient.connect(connectionString, {useUnifiedTopology: true})
+	.then(client => {
+		console.log('Connected to Database');
+		const db = client.db('todo');
+		app.use();
+		app.get();
+		app.post();
+		app.listen();
+	})
+	.catch(error => console.log(error));
 
 // Always place body parser before CRUD handlers!!
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
 // All handlers
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/index.html');
-	console.log("Hitting todo");
-})
+});
+
+app.post('/todo', (req, res) => {
+	console.log(req.body);
+});
 
 app.listen(3000, function(){
 	console.log('listening to port 3000');
