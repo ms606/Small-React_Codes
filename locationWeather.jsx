1cb8af36dc5de985bf3fcffdd7d20fb4d596887d
Mufaddal Shakir <muffi606@gmail.com>
2020-12-21 06:29:48 +0500

    Add files via upload

--- locationWeather.jsx
+++ locationWeather.jsx
@@ -1,7 +1,13 @@
 import React from "react"; 
 import PropTypes from "prop-types";
-import { CircularProgress, makeStyles } from "@material-ui/core";
-
+import { CircularProgress, makeStyles} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import Tooltip from "@material-ui/core/Tooltip";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+import ErrorIcon from "@material-ui/icons/Error";
+import getLocationWeather from "./getLocationWeather";
 
 
 const useStyles = makeStyles(theme => ({
@@ -29,4 +35,101 @@ const useStyles = makeStyles(theme => ({
 function LoadingIndicator({isLoading}){
     return isLoading ? <CircularProgress /> : null;
 };
+
+function ErrorMessage({apiError}){
+    if (!apiError) return null;
+
+    return (
+        <>
+          <ErrorIcon color="error" />
+          <Typography color="error" variant="h6">
+              {apiError}
+          </Typography>
+        </>  
+    );
+}
+
+function WeatherDisplay({weatherData}){
+    const classes = useStyles();
+    const {temp, description, icon, windTransform, windSpeed} = React.useMemo(() => {
+        const [weather] = weatherData.weather || [];
+        return {
+            temp: weatherData.main && weatherData.main.temp ? Math.round(weatherData.main.temp).toString() : "",
+            description: weather ? weather.description : "",
+            icon: weather ? `http://openweathermap.org/img/wn/${weather.icon}@2x.png` : "",
+            windTransform: weatherData.wind ? weatherData.wind.deg -  90 : null,
+            windSpeed: weatherData ? Math.round(weatherData.wind.speed) : 0,
+        }
+    }, [weatherData]);
+
+    return(
+        <>
+            {temp && <Typography variant="h6"></Typography>}
+            {icon && (
+                <Tooltip title={description} aria-label={description}>
+                    <Avatar className={classes.largeAvatar} alt={description} src={icon} />
+                </Tooltip>
+            )}
+            {windSpeed > 0 && (
+                <>
+                 <Typography variant="h6">{`${windSpeed} km/h`}</Typography>
+                 {windTransform !== null && (
+                     <ArrowRightAltIcon style={{transform: `rotateZ($(windTransform)deg)`}} />
+                 )}
+                </>
+            )}
+        </>
+    );
+};
+
+function locationWeather({location}) {
+    const classes = useStyles();
+
+    const [weatherData, setWeatherData] = React.useState({});
+    const [apiError, setApiError] = React.useState("");
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    React.useState(() => {
+        const LoadingIndicator =setTimeout(() => setIsLoading(true), 500);
+        const getWeather = async () => {
+            const result = await getLocationWeather(location);
+            clearTimeout(LoadingIndicatorTimeout);
+            setIsLoading(false);
+            setWeatherData(result.success ? "" : result.error);
+            setApiError(result.success ? "" : result.error);
+        };
+
+        getWeather();
+        return () => clearTimeout(LoadingIndicatorTimeout);  
+    }, [location]);
+
+    const { flagIcon, contryCode } = React.useMemo(() => {
+        return {
+            flagIcon: weatherData.sys ? `https://www.countryflags.io/${weatherData.sys.country}/shiny/32.png` : "",
+            countryCode: weatherData.sys ? weatherData.sys.country : "",
+        };
+    }, [weatherData]);
+
+    return (
+        <>
+            <div className={className.headerLine}>
+                <Typography className={classes.location} variant="h5">
+                    {location}
+                </Typography>
+                {flagIcon && <img alt={contryCode} src={flagIcon} />}
+            </div>
+            <div className={classes.detailLine}>
+                <LoadingIndicator isLoading={isLoading} />
+                <ErrorMessage apiError={apiError} />
+                <weatherDisplay weatherData={weatherData} />
+            </div>
+        </>
+    );
+}
+
+LocationWeather.propTypes = {
+    location: PropTypes.string.isRequired,
+};
+
+export default LocationWeather;
 
