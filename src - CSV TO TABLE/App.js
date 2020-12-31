4db0fd6276edb884901efc5901716529fa60ae4c
Mufaddal Shakir <muffi606@gmail.com>
2020-12-31 17:18:34 +0500

    Add files via upload

---
+++ src - CSV TO TABLE/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { CsvToHtmlTable } from 'react-csv-to-table';
+import { SampleData } from './SampleData';
+
+
+class MyTables extends
+
+
+
+function App() {
+
+  <form></form>
+
+
+  return (
+         
+  <CsvToHtmlTable data={SampleData} csvDelimiter="," 
+  tableClassName="table table-striped table-hover"/>
+
+  );
+}
+
+export default App;
