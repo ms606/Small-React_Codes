26e7d8ef684f30710942890b3c0b899273227f10
Mufaddal Shakir <muffi606@gmail.com>
2021-03-09 21:33:39 +0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,6 +4,12 @@ function reducer(state, action){
     switch(action.type){
         case "add-todo":
           return {todos: [...state.todos, { text: action.text, completed: false}] };
+        case "toggle-todo"  :
+          return {
+              todos: state.todos.map((t, idx) => 
+                idx === action.idx ? {...t, completed: !t.completed } : t 
+              )
+          } 
         default:
             return state;  
     }
@@ -13,25 +19,28 @@ const App = () => {
     const [{ todos }, dispatch] = useReducer(reducer, {todos: []});
     const [text, setText] = useState();
     
-    console.log(todos);
-
     return (
         <div>
             <form 
               onSubmit={ e => {
                 e.preventDefault();
-                dispatch({type: "add-todo", payload: text})
+                dispatch({type: "add-todo", text});
+                setText("");
                 
              }}
             >
                  <input value={text} 
                         onChange={e => setText(e.target.value)} />
             </form>
-            
-
-        
-            {todos.map(t => (
-                <div  key={t.text} > {t.text} </div>
+                    
+            {todos.map((t, idx) => (
+                <div  
+                    key={t.text} 
+                    onClick={() => dispatch({ type: "toggle-todo", idx })}
+                    style={
+                        {textDecorationLine: t.completed ? 'line-through' : ""}
+                    }
+                > {t.text} </div>
                 ))}
         
         </div>
@@ -39,4 +48,6 @@ const App = () => {
 }
 
 export default App;
+
+
 
